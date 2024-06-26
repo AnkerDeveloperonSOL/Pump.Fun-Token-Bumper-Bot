f964029faaccde7255c89cefade3ab8a09916aa0
AnkerDeveloperonSOL <ankerdeveloper@proton.me>
2024-06-26 13:12:43 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const RPC_URL = ""; // Quicknode or Helius give good rpc urls
+export const PRIVKEY = ""; // the private key of the account who will buy and sell, in base58 (phantom export for example)
+export const TOKEN_ADDR = ""; // Put the address of the token you want to bump here
+
+export const SOL_BUY_AMOUNT = 0.011; // here you can choose to increase/decrease the buy amount
+
+export const FEES = 0.0005; // here you can adjust the fees
+export const SLIPPAGE = 20; // here you can adjust the slippage
